c462b72ea41ed83b1e945539f8e61a3ffbd97d2b
Alex <rozenbaum1988@gmail.com>
2023-01-05 13:41:59 +0200

    wishlist

--- src/components/user/UserIcons.jsx
+++ src/components/user/UserIcons.jsx
@@ -1,58 +1,80 @@
-import { AddCircle, Dashboard, ShoppingBag, ShoppingCart } from '@mui/icons-material';
-import { Avatar, Badge, Box, IconButton, ListItemIcon, Tooltip } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import userStore from '../../store/userStore/userStore';
-import UserMenu from './UserMenu';
-import { observer } from 'mobx-react';
-import { useNavigate } from 'react-router-dom';
-import { toJS } from 'mobx';
-import authStore from '../../store/authStore/authStore';
+import {
+  AddCircle,
+  Dashboard,
+  ShoppingBag,
+  ShoppingCart,
+} from "@mui/icons-material";
+import {
+  Avatar,
+  Badge,
+  Box,
+  IconButton,
+  ListItemIcon,
+  Tooltip,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import userStore from "../../store/userStore/userStore";
+import UserMenu from "./UserMenu";
+import { observer } from "mobx-react";
+import { useNavigate } from "react-router-dom";
+import { toJS } from "mobx";
+import authStore from "../../store/authStore/authStore";
 const UserIcons = () => {
   useEffect(() => {
     authStore.checkUser();
-   },[]);
-   const authAdmin=(toJS(authStore.authAdmin)=== "true");
-   const authUser= (toJS(authStore.authUser)=== "true");
- const user=userStore.user;
-const wishlength= user.wishlist.length ;
-const lotlength= user.lotlist.length ;
+  }, []);
+  const authAdmin = toJS(authStore.authAdmin) === "true";
+  const authUser = toJS(authStore.authUser) === "true";
+  const user = userStore.user;
+  let wishlength = 0;
+  let lotlength = 0;
+  if (authUser) {
+    wishlength = user.wishlist.length;
+    lotlength = user.lotlist.length;
+  }
+
   const [anchorUserMenu, setAnchorUserMenu] = useState(null);
-const navigate=useNavigate();
+  const navigate = useNavigate();
   return (
-    <Box>{authUser?<>
-       <Tooltip title="Check your lotlist and bids">
-      <IconButton size="large" color="inherit" >
-        <Badge  badgeContent={lotlength?lotlength:""}>
-          <ShoppingBag onClick={() =>
-                navigate ('/user/mylotlist')
-             }/>
-        </Badge>
-      </IconButton>
-      </Tooltip>
-      <Tooltip title="Check your wishlist">
-      <IconButton size="large" color="inherit" >
-        <Badge  badgeContent={wishlength?wishlength:""}>
-          <ShoppingCart onClick={() =>
-                navigate ('/user/mywishlist')
-             }/>
-        </Badge>
-      </IconButton>
-      </Tooltip>
-      <Tooltip title="Add item for sell">
-      <IconButton size="large" color="inherit">
-        <Badge >
-          <AddCircle onClick={() =>
-                navigate ('/user/additem/')
-             }/>
-        </Badge>
-      </IconButton>
-      </Tooltip></>:<>
-      {authAdmin&&
-        <Tooltip title="Admin Dashboard"><ListItemIcon>
-            <Dashboard fontSize="small" onClick={() =>
-                navigate ('/admin/dashboard/')}  />
-          </ListItemIcon>
-          </Tooltip>}</>}
+    <Box>
+      {authUser ? (
+        <>
+          <Tooltip title="Check your lotlist and bids">
+            <IconButton size="large" color="inherit">
+              <Badge badgeContent={lotlength ? lotlength : ""}>
+                <ShoppingBag onClick={() => navigate("/user/mylotlist")} />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Check your wishlist">
+            <IconButton size="large" color="inherit">
+              <Badge badgeContent={wishlength ? wishlength : ""}>
+                <ShoppingCart onClick={() => navigate("/user/mywishlist")} />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Add item for sell">
+            <IconButton size="large" color="inherit">
+              <Badge>
+                <AddCircle onClick={() => navigate("/user/additem/")} />
+              </Badge>
+            </IconButton>
+          </Tooltip>
+        </>
+      ) : (
+        <>
+          {authAdmin && (
+            <Tooltip title="Admin Dashboard">
+              <ListItemIcon>
+                <Dashboard
+                  fontSize="small"
+                  onClick={() => navigate("/admin/dashboard/")}
+                />
+              </ListItemIcon>
+            </Tooltip>
+          )}
+        </>
+      )}
       <Tooltip title="Open User Settings">
         <IconButton onClick={(e) => setAnchorUserMenu(e.currentTarget)}>
           <Avatar src={user.img_url} alt={user.name}>
@@ -60,9 +82,9 @@ const navigate=useNavigate();
           </Avatar>
         </IconButton>
       </Tooltip>
-      {<UserMenu {...{ anchorUserMenu, setAnchorUserMenu }} /> }
+      {<UserMenu {...{ anchorUserMenu, setAnchorUserMenu }} />}
     </Box>
   );
 };
 
-export default observer (UserIcons);
+export default observer(UserIcons);
