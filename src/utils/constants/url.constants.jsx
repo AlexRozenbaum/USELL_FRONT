69eec57a4a8aa870f59ba64107e9f6b327d6bb28
Alex <rozenbaum1988@gmail.com>
2023-01-06 11:52:45 +0200

    api

--- src/utils/constants/url.constants.jsx
+++ src/utils/constants/url.constants.jsx
@@ -1,4 +1,4 @@
-export const API_URL = "https://ill-bear-gown.cyclic.app"
+export const API_URL = "https://average-pink-gloves.cyclic.app"
 export const TOKEN_KEY = "USELL_TOKEN"
 export const LOGGINED_USER="USER"
 export const LOGGINED_ADMIN="ADMIN"
